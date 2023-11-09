a4c929725893e60c3b262a5360721bbe564b82a4
Sadek Ali <sadekali1530@gmail.com>
2023-11-09 23:07:07 +0600

    set tost in wishlist add

--- src/Pages/AllBlig/card.jsx
+++ src/Pages/AllBlig/card.jsx
@@ -3,27 +3,40 @@ import { useContext } from "react";
 import { Link } from "react-router-dom";
 import { toast } from 'react-toastify';
 import { AuthContext } from "../../Auth/AuthProvider";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 
-const Card = ({blog}) => {
+const Card = ({ blog }) => {
 
-    const {user}=useContext(AuthContext)
-    const {sd, ld, name, title, img, date, type,  autorimg,_id}=blog;
-    const email=user?.email;
-    
+    const { user } = useContext(AuthContext)
+    const { sd, ld, name, title, img, date, type, autorimg, _id } = blog;
+    const email = user?.email;
 
-    const handllist=(blog)=>{
-        const myb={blog,email}
+
+    const handllist = (blog) => {
+        const myb = { blog, email }
         console.log(myb)
-        axios.post(`http://localhost:5001/wishlist`,myb)
-        .then(res=>{
-            console.log(res.data)
-        })
-       
+        axios.post(`http://localhost:5001/wishlist`, myb)
+            .then(res => {
+                console.log(res.data)
+                toast.success(' Added in wishlist', {
+                    position: "top-center",
+                    autoClose: 3000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                    theme: "light",
+                });
+
+            })
+
 
     }
 
-   
+
     return (
         <div className="card  bg-base-100 shadow-xl">
             <figure><img className="h-56" src={img} alt="Shoes" /></figure>
@@ -32,13 +45,26 @@ const Card = ({blog}) => {
                 <p>{title}</p>
                 <div className="card-actions justify-center">
                     <Link to={`/details/${_id}`} className="btn btn-outline btn-success ">Details</Link>
-                    <button onClick={ ()=>{handllist(blog)} } className="btn btn-outline btn-success " >Wishlist</button>
-                  
-                    
+                    <button onClick={() => { handllist(blog) }} className="btn btn-outline btn-success " >Wishlist</button>
+
+
                 </div>
             </div>
+
+            <ToastContainer
+                position="top-center"
+                autoClose={5000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+                theme="light"
+            />
         </div>
-        
+
     );
 };
 
